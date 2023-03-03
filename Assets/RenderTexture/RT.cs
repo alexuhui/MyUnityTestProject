f56c67efe86b210aa61923c8ff86033aff8cc840
chenyouhui1 <chenyouhui1@joyy.com>
2023-03-03 16:30:05 +0800

    rt

--- Assets/RenderTexture/RT.cs
+++ Assets/RenderTexture/RT.cs
@@ -11,6 +11,25 @@ public class RT : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine("Delay");
+    }
+
+    IEnumerator Delay()
+    {
+        rts = new RenderTexture[RawImages.Length];
+
+        rts[0] = RenderTexture.GetTemporary(1920, 1080);
+        RawImages[0].texture = rts[0];
+        Camera.main.targetTexture = rts[0];
+        yield return new WaitForSeconds(0.1f);
+
+
+        rts[1] = new RenderTexture(1920, 1080, 16);
+        RawImages[1].texture = rts[1];
+        Camera.main.targetTexture = rts[1];
+        yield return new WaitForSeconds(0.1f);
+
+        Camera.main.targetTexture = null;
     }
 
     // Update is called once per frame
@@ -21,42 +40,42 @@ public class RT : MonoBehaviour
 
     bool takePhoto = false;
     RenderTexture[] rts;
-    private void OnRenderImage(RenderTexture source, RenderTexture destination)
-    {
-        Graphics.Blit(source, destination);
-        if (takePhoto) return;
-        takePhoto = true;
-        Debug.Log($"---------------------- takePhoto {takePhoto}");
-        rts = new RenderTexture[RawImages.Length];
+//    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+//    {
+//        Graphics.Blit(source, destination);
+//        if (takePhoto) return;
+//        takePhoto = true;
+//        Debug.Log($"---------------------- takePhoto {takePhoto}");
+//        rts = new RenderTexture[RawImages.Length];
 
-        rts[0] = RenderTexture.GetTemporary(1920, 1080);
-#if DEBUG
-        rts[0].name = "rt 0";
-#endif
-        Graphics.Blit(source, rts[0]);
-        RawImages[0].texture = rts[0];
-        //rt1.Release();
-        //RenderTexture.ReleaseTemporary(rt1);
+//        rts[0] = RenderTexture.GetTemporary(1920, 1080);
+//#if DEBUG
+//        rts[0].name = "rt 0";
+//#endif
+//        Graphics.Blit(source, rts[0]);
+//        RawImages[0].texture = rts[0];
+//        //rt1.Release();
+//        //RenderTexture.ReleaseTemporary(rt1);
 
 
-        rts[1] = RenderTexture.GetTemporary(1920, 1080);
-        rts[1].name = "rt 1";
-        Graphics.Blit(source, rts[1]);
-        // rt2.Release();
-        RawImages[1].texture = rts[1];
+//        rts[1] = RenderTexture.GetTemporary(1920, 1080);
+//        rts[1].name = "rt 1";
+//        Graphics.Blit(source, rts[1]);
+//        // rt2.Release();
+//        RawImages[1].texture = rts[1];
 
-        rts[2] = RenderTexture.GetTemporary(1920, 1080);
-        rts[2].name = "rt 2";
-        Graphics.Blit(source, rts[2]);
-        RawImages[2].texture = rts[2];
-        //rt3.Release();
-        //Destroy(rt3);
+//        rts[2] = RenderTexture.GetTemporary(1920, 1080);
+//        rts[2].name = "rt 2";
+//        Graphics.Blit(source, rts[2]);
+//        RawImages[2].texture = rts[2];
+//        //rt3.Release();
+//        //Destroy(rt3);
 
-        rts[3] = new RenderTexture(1920, 1080, 16);
-        rts[3].name = "rt 3";
-        Graphics.Blit(source, rts[3]);
-        RawImages[3].texture = rts[3];
-    }
+//        rts[3] = new RenderTexture(1920, 1080, 16);
+//        rts[3].name = "rt 3";
+//        Graphics.Blit(source, rts[3]);
+//        RawImages[3].texture = rts[3];
+//    }
 
     public void OnGCClick()
     {
